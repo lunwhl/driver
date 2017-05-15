bae4b3a57f64c679decdc1ac1508aa5c0dd60a2c
lunwhl <lunwhl@hotmail.com>
2017-05-15 21:55:28 +0800

    location

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/map/geocoding', 'DeliveryController@getGeoByCoordinate');
+Route::get('/map/distance', 'DeliveryController@getDistance');
+Route::get('/map/geolocation','DeliveryController@getGeolocation');
