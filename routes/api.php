0a2f81de55332eb039dfedded5459b68d25b8088
lunwhl <lunwhl@hotmail.com>
2017-06-25 14:35:37 +0800

    change to post api

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/find/driver', 'DeliveryController@getPotentialDriver');
+Route::post('/find/driver', 'DeliveryController@getPotentialDriver');
